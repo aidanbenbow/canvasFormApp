27c7b3872e4678e48f5b9fae53f83fe22845bdc0
Aidan Benbow <aidanbenbow@gmail.com>
2025-09-08 10:41:07 +0300

    logs

--- server.js
+++ server.js
@@ -14,13 +14,17 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 
-app.use(express.static('public'));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  console.log(`[${req.method}] ${req.url}`);
+  next();
+});
 
 app.use('/', indexRoutes);
-
+app.use(express.static('public'));
 app.listen(4500, () => {
   console.log("Server is running on port 4500");
 });
