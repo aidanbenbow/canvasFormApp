7f2eeb0c3aca6ef4ec5a423139f4b07fd0328c6b
Aidan Benbow <aidanbenbow@gmail.com>
2025-09-15 09:06:44 +0300

    io

--- public/socketClient.js
+++ public/socketClient.js
@@ -3,7 +3,7 @@ import { io } from "https://cdn.jsdelivr.net/npm/socket.io-client@4.7.2/+esm";
 
 
 //const socket = io("http://localhost:4500"); // Replace with your server URL
-const socket = io("http://18.199.154.27"); // ✅ Include port if backend is on 4500
+const socket = io(); // ✅ Include port if backend is on 4500
 socket.on('connect', () => {
   console.log('Connected to WebSocket server:', socket.id);
 });
